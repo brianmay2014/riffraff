935dd96a32754a310f1b69624dde98c5dc7d61ac
Brian May <brianmay2014@gmail.com>
2022-06-10 10:27:12 -0700

    sign-up error validation complete

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
@@ -19,9 +19,14 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def username_length(form, field):
+    # Checking if username is already in use
+    username = field.data
+    if len(username) < 4 or len(username) > 16:
+        raise ValidationError('Username must be between 4 and 16 characters.')
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+        'username', validators=[DataRequired(), username_exists, username_length])
+    email = StringField('email', validators=[DataRequired(), Email("Please enter a valid e-mail address."), user_exists])
     password = StringField('password', validators=[DataRequired()])
