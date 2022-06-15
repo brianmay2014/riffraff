e9d04de21180ec77b2b3428ebfb85b97cf216941
Brian May <brianmay2014@gmail.com>
2022-06-15 10:42:55 -0700

    test for heroku upload issue

--- app/utils/s3utils.py
+++ app/utils/s3utils.py
@@ -42,6 +42,7 @@ def upload_file_to_s3(file, acl="public-read"):
         )
     except Exception as e:
         # in case the our s3 upload fails
-        return {"errors": str(e)}
+        # return {"errors": str(e)}
+        return {"errors": str(e), "bucket": str(BUCKET_NAME)}
 
     return {"url": f"{S3_LOCATION}{file.filename}"}
