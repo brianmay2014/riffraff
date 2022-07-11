e57354a678703ab84bfd12194a94b4de66237f9f
Brian May <brianmay2014@gmail.com>
2022-07-11 10:16:38 -0700

    following buttons work with functionality across user pages

--- react-app/src/components/UserPage/UserPage.js
+++ react-app/src/components/UserPage/UserPage.js
@@ -16,6 +16,7 @@ const UserPage = () => {
 	// const [errors, setErrors] = useState([]);
     const riffs = useSelector((state) => state.riffs);
     const user = useSelector((state) => state.user);
+    const follows = useSelector((state) => state.follows);
     const [errors, setErrors] = useState([]);
 	// const comments = useSelector((state) => state.comments);
 
@@ -26,6 +27,7 @@ const UserPage = () => {
         dispatch(genRiffs());
 		dispatch(genComments());
         dispatch(genUser(parseInt(userId, 10)))
+        dispatch(genFollows())
     }, [dispatch, userId]);
 
 
@@ -45,124 +47,168 @@ const UserPage = () => {
 
 			return;
 		}
+
+        //generate following list after follow is successful
+        const genfollow = await dispatch(genFollows());
+
+		if (genfollow.errors) {
+			setErrors(genfollow.errors);
+			genfollow.errors = [];
+
+			return;
+		}
+
+
     }
 
     const unfollowSubmit = async (e) => {
 		e.preventDefault();
 
-        const followed_id = parseInt(userId);
-        const follower_id = user.id;
+		const followed_id = parseInt(userId);
+		const follower_id = user.id;
 
-        setErrors([]);
+		setErrors([]);
 
-        const unfollow = await dispatch(deleteFollow(followed_id, follower_id));
+		const unfollow = await dispatch(deleteFollow(followed_id, follower_id));
 
-        if (unfollow.errors) {
-            setErrors(unfollow.errors);
-            unfollow.errors = [];
+		if (unfollow.errors) {
+			setErrors(unfollow.errors);
+			unfollow.errors = [];
 
-            return;
-        }
+			return;
+		}
+
+		//generate following list after unfollow is successful
+		const genfollow = await dispatch(genFollows());
+
+		if (genfollow.errors) {
+			setErrors(genfollow.errors);
+			genfollow.errors = [];
+
+			return;
+		}
 	};
 
-        const generateFollows = async (e) => {
-			e.preventDefault();
+        // function for testing follow actions
+        // const generateFollows = async (e) => {
+		// 	e.preventDefault();
 
-			// const followed_id = parseInt(userId);
-			// const follower_id = user.id;
+		// 	// const followed_id = parseInt(userId);
+		// 	// const follower_id = user.id;
 
-			// setErrors([]);
+		// 	// setErrors([]);
 
-			const genfollow = await dispatch(
-				genFollows()
-			);
+		// 	const genfollow = await dispatch(
+		// 		genFollows()
+		// 	);
 
-			if (genfollow.errors) {
-				setErrors(genfollow.errors);
-				genfollow.errors = [];
+		// 	if (genfollow.errors) {
+		// 		setErrors(genfollow.errors);
+		// 		genfollow.errors = [];
 
-				return;
-			}
-		};
+		// 		return;
+		// 	}
+		// };
+        
+        
+        // const user = {id: 1,
+        //     "email": "demo@demo.com",
+        // "username": "demoriffer",
+        // "password": "password",
+        // "pic_url": "",
+        // "bio": "demo user for riffraff" };
+        
+        const followArr = follows[currentUser.id]
+        // console.log(followArr);
 
-
-    // const user = {id: 1,
-    //     "email": "demo@demo.com",
-    // "username": "demoriffer",
-    // "password": "password",
-    // "pic_url": "",
-    // "bio": "demo user for riffraff" };
-    
     // null before riffs loads from the store
-    if (!riffs && !user) {
+    if (!riffs && !user && !followArr) {
         return null;
      } else {
+			if (
+				user?.pic_url === "" ||
+				user?.pic_url === null ||
+				user?.pic_url === undefined
+			) {
+				user.pic_url =
+					"https://images.pexels.com/photos/7899456/pexels-photo-7899456.png?auto=compress&cs=tinysrgb&w=126&h=75&dpr=1";
+			}
+
+			console.log(user);
+
+			// console.log(riffs);
+			const riffArrUnfiltered = Object.values(riffs);
+
+			const riffArr = riffArrUnfiltered.filter((riff) => {
+				// console.log(riff)
+				return riff.user_id == userId;
+			});
+			// console.log('user.id', user.id)
+			// console.log(' type of user.id', typeof(user.id))
+			// console.log('userId', userId)
+			// console.log(' type of userId', typeof(userId))
+			// console.log(currentUser);
+			// console.log(riffArr);
+
+			//sort by id - to show newest created at the top
+			riffArr.sort((a, b) => {
+				return b.id - a.id;
+			});
 
 
-        if (user?.pic_url === '' || user?.pic_url === null || user?.pic_url === undefined) {
-			user.pic_url =
-			"https://images.pexels.com/photos/7899456/pexels-photo-7899456.png?auto=compress&cs=tinysrgb&w=126&h=75&dpr=1";
-		}
-        
-        console.log(user);
-        
-		// console.log(riffs);
-		const riffArrUnfiltered = Object.values(riffs)
+			return (
+				<div id="user-page">
+					<div id="user-display">
+						<div id="user-display-left">
+							<img src={user.pic_url}></img>
+						</div>
+						<div id="user-display-right">
+							<h3>{user.username}</h3>
+							<p id="user-bio">{user.bio}</p>
+						</div>
 
-        const riffArr = riffArrUnfiltered.filter((riff) => {
-            // console.log(riff)
-            return riff.user_id == userId;
-        })
-        // console.log('user.id', user.id)
-        // console.log(' type of user.id', typeof(user.id))
-        // console.log('userId', userId)
-        // console.log(' type of userId', typeof(userId))
-        // console.log(currentUser);
-        // console.log(riffArr);
-        
-		//sort by id - to show newest created at the top
-		riffArr.sort((a, b) => {
-            return b.id - a.id;
-		});
-		
-		return (
-			<div id="user-page">
-				<div id="user-display">
-					<div id="user-display-left">
-						<img src={user.pic_url}></img>
-					</div>
-					<div id="user-display-right">
-						<h3>{user.username}</h3>
-						<p id="user-bio">{user.bio}</p>
-					</div>
+						{/* FOLLOW AND EDIT BUTTONS */}
+						<div id="user-display-buttons">
+							{userId != currentUser.id &&
+								!followArr?.includes(parseInt(userId, 10)) && (
+									<form
+										id="follow-button"
+										onSubmit={followSubmit}
+									>
+										<button className="btn" type="submit">
+											Follow
+										</button>
+									</form>
+								)}
+							{userId != currentUser.id &&
+								followArr?.includes(parseInt(userId, 10)) && (
+									<form
+										id="unfollow-button"
+										onSubmit={unfollowSubmit}
+									>
+										<button className="btn" type="submit">
+											Unfollow
+										</button>
+									</form>
+								)}
+							{/* <button className="btn" onClick={generateFollows}>
+								Check follows
+							</button> */}
 
-					{/* FOLLOW AND EDIT BUTTONS */}
-					<div id="user-display-buttons">
-						<form id="follow-button" onSubmit={followSubmit}>
-							<button className="btn" type="submit">
-								Follow
-							</button>
-						</form>
-						<form id="unfollow-button" onSubmit={unfollowSubmit}>
-							<button className="btn" type="submit">
-								Unfollow
-							</button>
-						</form>
-						<button className="btn" onClick={generateFollows}>
-							Check follows
-						</button>
-
-						{/* <button id="follow-button" className="btn">
+							{/* <button id="follow-button" className="btn">
 							Follow
 						</button> */}
 
-						{userId == currentUser.id && (
-							<button id="edit-profile-button" className="btn">
-								Edit Profile
-							</button>
-						)}
+							{userId == currentUser.id && (
+								<button
+									id="edit-profile-button"
+									className="btn"
+								>
+									Edit Profile
+								</button>
+							)}
 
-						{/* COMMENT ME OUT
+							{/* COMMENT ME OUT
 						{showRiffModal && (
 							<Modal
 								onClose={() => {
@@ -177,22 +223,24 @@ const UserPage = () => {
 						)} COMMENT ABOVE ME
                         
                     FUTURE FEATURES */}
+						</div>
+					</div>
+					<h5 id="riff-label">{user.username}'s riffs</h5>
+
+					<div id="feed-body">
+						{riffArr &&
+							riffArr.map((riff) => {
+								return (
+									<RiffCard
+										key={`key-${riff?.id}`}
+										riff={riff}
+									/>
+								);
+							})}
 					</div>
 				</div>
-				<h5 id="riff-label">{user.username}'s riffs</h5>
-
-				<div id="feed-body">
-					{riffArr &&
-						riffArr.map((riff) => {
-							return (
-								<RiffCard key={`key-${riff?.id}`} riff={riff} />
-							);
-						})}
-				</div>
-			</div>
-		);
-                
-            };
+			);
+		};
             
         };
 
