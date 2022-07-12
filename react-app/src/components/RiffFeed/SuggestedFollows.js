51100d2fa5dfeada6e75be3875f3e32a1455cbda
Brian May <brianmay2014@gmail.com>
2022-07-11 17:17:59 -0700

    rows populated, follow button follows - need re-render

--- react-app/src/components/RiffFeed/SuggestedFollows.js
+++ react-app/src/components/RiffFeed/SuggestedFollows.js
@@ -1,22 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-// import { Redirect, useHistory } from "react-router-dom";
-// import { login } from "../../store/session";
 import "./RiffFeed.css";
 import { genUnfollows } from "../../store/unfollow";
-import RiffCard from "./RiffCard";
+import { makeFollow, deleteFollow, genFollows } from "../../store/follow";
 
 const SuggestedFollows = () => {
 	// const [errors, setErrors] = useState([]);
 	const currentUser = useSelector((state) => state.session.user);
 	const unfollows = useSelector((state) => state.unfollows);
+    const follows = useSelector((state) => state.follows);
 	// const comments = useSelector((state) => state.comments);
 
 	const dispatch = useDispatch();
 	// const history = useHistory();
 
 	useEffect(() => {
-        console.log('inside the use effect for unfollooolllloowwsss----------------')
+        // console.log('inside the use effect for unfollooolllloowwsss----------------')
 		dispatch(genUnfollows());
                  
 	}, [dispatch]);
@@ -29,52 +28,84 @@ const SuggestedFollows = () => {
 	// 	return null;
 	// }
 
-    console.log(unfollows);
+    // console.log(unfollows);
     let unfollowArr = Object.values(unfollows);
-    console.log(unfollowArr)
+    // console.log(unfollowArr)
 
 	return (
 		<div id="suggested-body">
-            <p> Just here to hang out</p>
+            <p>Suggestions For You</p>
             {unfollowArr && unfollowArr.map((user) => {
-                return <li>{user.username}</li>;
+                // return <p>{user.username}</p>
+                return <FollowRow key={`key-${user?.id}`} user={user} />;
             })}
 		</div>
 	);
 };
 
-export default SuggestedFollows;
+
+const FollowRow = (user) => {
+    
+    // console.log(user.user);
+    let rowuser = user.user
+    const currentUser = useSelector((state) => state.session.user);
+    const [errors, setErrors] = useState([]);
+
+    const dispatch = useDispatch();
+
+        const followSubmit = async (e) => {
+			e.preventDefault();
+
+			const followed_id = rowuser.id;
+			const follower_id = currentUser.id;
+
+			setErrors([]);
+
+			const follow = await dispatch(makeFollow(followed_id, follower_id));
+
+			if (follow.errors) {
+				setErrors(follow.errors);
+				follow.errors = [];
+
+				return;
+			}
+
+			//generate following list after follow is successful
+			const genfollow = await dispatch(genFollows());
+
+			if (genfollow.errors) {
+				setErrors(genfollow.errors);
+				genfollow.errors = [];
+
+				return;
+			}
+		};
 
 
 
-const SuggestedFollows = () => {
-	// const [errors, setErrors] = useState([]);
-	const currentUser = useSelector((state) => state.session.user);
-	const unfollows = useSelector((state) => state.unfollows);
-	// const comments = useSelector((state) => state.comments);
 
-	const dispatch = useDispatch();
-	// const history = useHistory();
-
-	useEffect(() => {
-        console.log('inside the use effect for unfollooolllloowwsss----------------')
-		dispatch(genUnfollows());
-                 
-	}, [dispatch]);
-
-
-    console.log(unfollows);
-    let unfollowArr = Object.values(unfollows);
-    console.log(unfollowArr)
+    		if (
+				rowuser.pic_url === "" ||
+				rowuser.pic_url === null ||
+				rowuser.pic_url === undefined
+			) {
+				rowuser.pic_url =
+					"https://images.pexels.com/photos/7899456/pexels-photo-7899456.png?auto=compress&cs=tinysrgb&w=126&h=75&dpr=1";
+			}
 
 	return (
-		<div id="suggested-body">
-            <p> Just here to hang out</p>
-            {unfollowArr && unfollowArr.map((user) => {
-                return <li>{user.username}</li>;
-            })}
+		<div className="follow-row">
+			<img className="card-user-img" src={rowuser.pic_url} alt="follow" />
+			{/* <p> {rowuser.pic_url} </p> */}
+			<a href={`/users/${rowuser.id}`}> {rowuser.username} </a>
+			<form id="follow-button" onSubmit={followSubmit}>
+				<button className="btn" type="submit">
+					Follow
+				</button>
+			</form>
 		</div>
 	);
 };
 
 export default SuggestedFollows;
+// export default FollowRow;
--- react-app/src/store/unfollow.js
+++ react-app/src/store/unfollow.js
@@ -1,21 +1,4 @@
-// const ADD_FOLLOW = "follows/addFollow";
-// const REMOVE_FOLLOW = "follows/removeFollow";
 const LOAD_UNFOLLOWS = "unfollows/loadUnfollows";
-
-// const addFollow = (follow) => {
-// 	// console.log('inside action');
-// 	return {
-// 		type: ADD_FOLLOW,
-// 		payload: follow,
-// 	};
-// };
-
-// const removeFollow = (follow) => {
-// 	return {
-// 		type: REMOVE_FOLLOW,
-// 		payload: follow,
-// 	};
-// };
 
 const loadUnfollows = (unfollows) => {
     console.log(unfollows);
@@ -25,50 +8,9 @@ const loadUnfollows = (unfollows) => {
 	};
 };
 
-// export const makeFollow = (followed_id, follower_id) => async (dispatch) => {
-// 	const form = new FormData();
-// 	// form.append("user_id", follow.user_id);
-// 	// form.append("riff_id", riffId);
-// 	// form.append("text", follow.text);
-
-// 	const response = await fetch(`/api/users/follow/${followed_id}/`, {
-// 		method: "POST",
-// 		body: form,
-// 	});
-
-// 	const followData = await response.json();
-
-// 	if (response.ok) {
-// 		dispatch(addFollow(followData));
-// 		return followData;
-// 	} else {
-// 		return followData;
-// 	}
-// };
-
-// export const deleteFollow = (followed_id, follower_id) => async (dispatch) => {
-// 	const form = new FormData();
-
-// 	const response = await fetch(`/api/users/unfollow/${followed_id}/`, {
-// 		method: "POST",
-// 		body: form,
-// 	});
-
-// 	const followData = await response.json();
-
-// 	if (response.ok) {
-// 		dispatch(removeFollow(followData));
-// 		return followData;
-// 	} else {
-// 		return followData;
-// 	}
-// };
 
 export const genUnfollows = () => async (dispatch) => {
 	// can add more types of responses later
-
-    console.log('think thunk thuuuuunk--------------')
-
 
 	const [unfollowsResponse] = await Promise.all([
 		fetch(`/api/users/unfollows/`, {
@@ -77,11 +19,11 @@ export const genUnfollows = () => async (dispatch) => {
 		}),
 	]);
 
-    console.log(unfollowsResponse);
+    // console.log(unfollowsResponse);
 
 	const [unfollows] = await Promise.all([unfollowsResponse.json()]);
 
-	console.log(unfollows);
+	// console.log(unfollows);
 
 	if (unfollowsResponse.ok) {
 		dispatch(loadUnfollows(unfollows));
@@ -91,12 +33,6 @@ export const genUnfollows = () => async (dispatch) => {
 
 const unfollowReducer = (state = {}, action) => {
 	switch (action.type) {
-		// case ADD_FOLLOW:
-		// 	return { ...state, [action.payload.id]: action.payload };
-		// case REMOVE_FOLLOW:
-		// 	const copyState = { ...state };
-		// 	delete copyState[action.payload.id];
-		// 	return copyState;
 		case LOAD_UNFOLLOWS:
 			const unfollowData = {};
             for (let user of action.payload) {
