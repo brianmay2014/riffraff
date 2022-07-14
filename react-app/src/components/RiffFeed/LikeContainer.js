9f09e9e37fc6ffec1a6fe53dca888ac6e3ed8b90
Brian May <brianmay2014@gmail.com>
2022-07-13 17:39:40 -0700

    added text for number of likes, needs styling fine tuning

--- react-app/src/components/RiffFeed/LikeContainer.js
+++ react-app/src/components/RiffFeed/LikeContainer.js
@@ -10,29 +10,51 @@ import "./RiffFeed.css";
 
 const LikeContainer = ({ riff }) => {
 	const user = useSelector((state) => state.session.user);
-	
+	const dispatch = useDispatch();
 
     // maybe can feed just the riff id
     // then have the component re-render and pull only that specific riff?
 
+    const submitLike = async (e) => {
+        e.preventDefault();
 
+        // dispatch(likeRiff(user.id, riff.id))
+
+        console.log('trying to like this')
+
+    }
+
+    const submitUnlike = async (e) => {
+		e.preventDefault();
+
+		// dispatch(unlikeRiff(user.id, riff.id))
+
+		console.log("trying to NOT like this");
+	};
 
 	// null before riffs / riff prop loads from the store
 	if (!riff) {
 		return null;
 	}
 
+    const likeCount = riff?.user_likes.length
+	
+
 	return (
 		<div id={`${riff?.id}-likes`} className="like-display-container">
-			
-            {!riff?.user_likes.includes(user.id) && <button className='like-button'>
-                <i className="fa-regular fa-heart"></i>
-            </button>}
-            {riff?.user_likes.includes(user.id) && <button className='like-button'>
-			    <i className="fa-solid fa-heart"></i>
-
-            </button>}
-
+			{!riff?.user_likes.includes(user.id) && (
+				<button onClick={submitLike} className="like-button">
+					<i className="fa-regular fa-heart"></i>
+				</button>
+			)}
+			{riff?.user_likes.includes(user.id) && (
+				<button onClick={submitUnlike} className="like-button">
+					<i className="fa-solid fa-heart"></i>
+				</button>
+			)}
+			{likeCount === 0 && <p>No likes</p>}
+			{likeCount === 1 && <p>1 like</p>}
+			{likeCount > 1 && <p>{likeCount} likes</p>}
 		</div>
 	);
 };
